43bc41f24ed4bdc6410281a4736b0a104c45d459
angelo <aurtula1@gmail.com>
2022-01-23 23:50:42 -0500

    commit tree ca589bfc6760f6a599d3debed66404cfb1d66d59

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
 export function manageFriends(state, action){
+    state = {
+        friends: []
+    }
+    switch(action.type){
+        case "ADD_FRIEND":
+            return {friends: state.friends + 1}
+            default:
+                return state
+    }
+
 }
